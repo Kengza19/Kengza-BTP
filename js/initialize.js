0ff8ca2f850ba84446bb2d8332909b3fcdd26bf7
Kengza <gwladys.kelodjou@gmail.com>
2020-04-29 10:02:54 +0100

    Building Landing Page

---
+++ js/initialize.js
@@ -0,0 +1,56 @@
+(function($){
+  $(function(){
+    
+    //initialisation du sidenav
+    $('.sidenav').sidenav();
+    /* $('.parallax').parallax();*/
+    
+    /*$('.carousel').carousel({
+      fullWidth: true
+    });
+    instance.next(3);*/
+    
+    //initialisation du slider
+    $('.slider').slider({
+      indicators: true,
+      height: 600
+    });
+    
+    //initialisation du parallax
+    $('.parallax').parallax();
+    
+    //initialisation du carousel
+    
+    $('.carousel').carousel({
+      shift: 200,
+      padding: -300
+    });
+    
+    //initialisation material box
+    
+    $('.materialboxed').materialbox();
+    
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+var navContact = document.getElementById('navContact'),
+navRealisation = document.getElementById('navRealisation'),
+navService = document.getElementById('navService;');
+
+
+navContact.addEventListener('click', function(){
+  navContact.nextSibling.style.color = "#EEC400"
+});
+
+
+$(document).ready(function() {
+  $('.galerie-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    cssEase: 'linear',
+    dots: false
+  });
+});
